065d3f815327718d6db6e49c611cbd7cb5204c6b
devprotack <desenvolvimento@protack.com.br>
2023-08-16 16:23:00 -0300

    Production configuration

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,28 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import Product from '../typeorm/entities/Product';
+import RedisCache from '@shared/cache/RedisCache';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const redisCache = new RedisCache();
+
+    let products = await redisCache.recover<Product[]>(
+      'api-vendas-PRODUCT_LIST',
+    );
+
+    if (!products) {
+      products = await productsRepository.find();
+
+      await redisCache.save('api-vendas-PRODUCT_LIST', products);
+    }
+
+    //const products = await productsRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
